f166570a3d138c5a980325017db4ac003af31d63
kkpa <cristiancamilopena@gmail.com>
2018-07-14 12:53:53 -0500

    Eliminacion del proyecto

--- Java/HackerRankSolutions/src/main/java/com/kkpa/hackerrank/velocity/Base.java
+++
@@ -1,7 +0,0 @@
-package com.kkpa.velocity;
-
-public interface Base {
-
-	boolean m1();
-	byte m2(short s);
-}
--- Java/HackerRankSolutions/src/main/java/com/kkpa/hackerrank/velocity/Class2.java
+++
@@ -1,9 +0,0 @@
-package com.kkpa.velocity;
-
-abstract class Class2 implements Base {
-
-	
-	public boolean m1() {
-		return 5 > 7;
-	}
-}
