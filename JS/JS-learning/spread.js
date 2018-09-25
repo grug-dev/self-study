0eefb36df59e367dfffe1009764d4e21fcbe0956
kkpa <cristiancamilopena@gmail.com>
2018-09-25 15:07:11 -0500

    Learning JS

--- JS/JS-learning/spread.js
+++ JS/JS-learning/spread.js
@@ -1,7 +1,29 @@
-sum = (x , y , z ) => x + y + z;
+sum = (x, y, z) => x + y + z;
 
-let numbers = [1 , 3];
+let numbers = [1, 3];
 
-numbers = [...numbers , 4 , 5]
+numbers = [...numbers, 4, 5]
 
 console.log(sum(...numbers));
+
+// Spread oBJECT
+let obj1 = {
+    name: 'Cristian'
+};
+let obj2 = {
+    name: 'Camilo'
+};
+let obj3 = {
+    ...obj2,
+    age: 29
+};
+
+createArrayObjets = (...objs) => {
+    let myobjects = [];
+    myobjects = objs;
+    return myobjects;
+}
+
+
+let myobjects = createArrayObjets(obj1, obj2, obj3);
+console.log(myobjects);
